221d0089d160c41adcc896de0984a2c3a0782c93
passerby-xa <aung.xu@gmail.com>
2018-03-16 08:54:33 +0800

    emap 1.0.2, MyMapOnTouchListener内部类

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # EMap
 EMap demo
+
+====在线emap的依赖库=====
+外层build.gradle添加maven库：
+ maven {
+		url  "https://dl.bintray.com/passerby-xa/EMap"
+       }
+
+内层build.gradle增加emap的lib依赖	   
+	compile 'com.gis.lib:lib:1.0.1'   
+	
+-------------------------3.16更新--------------------------------
+1，emap版本更新至1.0.2，compile 'com.gis.lib:lib:1.0.2'
+2，demo中MainActivity中增加了MyMapOnTouchListener内部类，一些点击操作，比如获取经纬度和点中了哪个点等常用操作，看onSingleTap方法中的示例代码
+3，地图事件的变更，具体看MainActivity即可
--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.esri.android.map.Callout;
 import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.MapOnTouchListener;
 import com.esri.android.map.MapView;
+import com.esri.core.geometry.Point;
 import com.esri.core.map.Graphic;
 import com.esri.core.symbol.SimpleLineSymbol;
 import com.esri.core.symbol.SimpleMarkerSymbol;
@@ -34,8 +37,10 @@ public class MainActivity extends Activity implements DrawEventListener {
     private Graphic selectGraphic = null;
     private DrawTool drawTool;
     Callout callout;
-    public MapOnTouchListener mapDefaultOnTouchListener;//默认点击事件
-    public DrawEventListener drawEventListener;//要素绘制点击事件
+    public MyMapOnTouchListener mapDefaultOnTouchListener;//点击事件
+
+    TrackTool tool;
+    TrackTool tool2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +49,7 @@ public class MainActivity extends Activity implements DrawEventListener {
 
         context = this;
         this.mapView = (MapView)this.findViewById(R.id.map);
-        EMap emap=new EMap(mapView);
+        emap=new EMap(mapView);
 
         emap.showWorldSatelliteLayer();//天地图影像
         // emap.showWorldVectorLayer();//天地图矢量
@@ -64,7 +69,7 @@ public class MainActivity extends Activity implements DrawEventListener {
 
         //轨迹线
         SimpleMarkerSymbol mRedMarkerSymbol = new SimpleMarkerSymbol(Color.RED, 15, SimpleMarkerSymbol.STYLE.CIRCLE);
-        TrackTool tool=new TrackTool(120.10123, 30.321, mRedMarkerSymbol, new SimpleLineSymbol(Color.BLACK, 5),true, mapView);
+        tool=new TrackTool(120.10123, 30.321, mRedMarkerSymbol, new SimpleLineSymbol(Color.BLACK, 5),true, mapView);
         tool.add(122.123321,30.1523);
         tool.add(124.123321,31.1523);
         tool.add(123.123321,32.1523);
@@ -74,7 +79,7 @@ public class MainActivity extends Activity implements DrawEventListener {
             Log.e("RouteInfo",list.get(i).getX()+","+list.get(i).getY()+","+list.get(i).getX1()+","+list.get(i).getY1()+","+list.get(i).getDistance());
         }
 
-        TrackTool tool2=new TrackTool(119.10123, 29.321, mRedMarkerSymbol, new SimpleLineSymbol(Color.BLUE, 5),true, mapView);
+        tool2=new TrackTool(119.10123, 29.321, mRedMarkerSymbol, new SimpleLineSymbol(Color.BLUE, 5),true, mapView);
         tool2.add(120.123321,30.1523);
         tool2.add(122.123321,32.1523);
         tool2.add(121.123321,32.1523);
@@ -90,20 +95,19 @@ public class MainActivity extends Activity implements DrawEventListener {
         this.drawTool.addEventListener(this);
 
         //设置地图事件
-        mapDefaultOnTouchListener = new MapOnTouchListener(emap.getMap().getContext(), emap.getMap());
-        drawEventListener = this;
+        mapDefaultOnTouchListener = new MyMapOnTouchListener(emap.getMap().getContext(), emap.getMap());
+        emap.setOnTouchListener(mapDefaultOnTouchListener);
 
         ToolsOnClickListener toolsOnClickListener = new ToolsOnClickListener(context,drawTool,selectGraphic,emap.getMap());
 
+        Button btnDrawPoint = (Button)this.findViewById(R.id.btnDrawPoint);
+        btnDrawPoint.setOnClickListener(toolsOnClickListener);
 
         Button btnDrawPolyline = (Button)this.findViewById(R.id.btnDrawPolyline);
         btnDrawPolyline.setOnClickListener(toolsOnClickListener);
 
-
         Button btnDrawPolygon = (Button)this.findViewById(R.id.btnDrawPolygon);
         btnDrawPolygon.setOnClickListener(toolsOnClickListener);
-
-
 
         Button btnDrawCircle = (Button)this.findViewById(R.id.btnDrawCircle);
         btnDrawCircle.setOnClickListener(toolsOnClickListener);
@@ -123,5 +127,38 @@ public class MainActivity extends Activity implements DrawEventListener {
     }
 
 
+    class MyMapOnTouchListener extends MapOnTouchListener {
+        public MyMapOnTouchListener(Context context, MapView view) {
+            super(context, view);
+        }
+        public boolean onSingleTap(MotionEvent e) {
+            Point point = mapView.toMapPoint(e.getX(), e.getY());
+            //绘点
+            emap.addPoint(point.getX(), point.getY(), new SimpleMarkerSymbol(Color.RED, 20, SimpleMarkerSymbol.STYLE.CIRCLE));
+            Log.e("点击", point.getX() + "," + point.getY()); //点击的经纬度
+            int idx = tool.getSelectedIndex(e.getX(), e.getY());  //点中了轨迹线中的第几个点，返回-1则表示一个也没有点中
+            int idx2 = tool2.getSelectedIndex(e.getX(), e.getY());
+            if (idx != -1) {
+                Log.e("点击了tool的第", idx + "个点！");
+                Toast.makeText(mapView.getContext(), "点击了tool的第"+idx + "个点！", Toast.LENGTH_SHORT).show();
+
+                /*
+                *******
+                点中以后的一些操作，比如页面跳转等
+                ********
+                */
+            }else{
+                if (idx2 != -1) {
+                    Log.e("点击了tool2的第", idx2 + "个点！");
+                    Toast.makeText(mapView.getContext(), "点击了tool2的第"+idx2 + "个点！", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(mapView.getContext(), "点击了："+point.getX() + "," + point.getY(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            return super.onSingleTap(e);
+        }
+
+    }
 
 }
