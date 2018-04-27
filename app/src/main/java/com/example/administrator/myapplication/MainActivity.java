6aa19437a83947022f92e10d2cae3e860ccc6bf1
passerby-xa <aung.xu@gmail.com>
2018-04-27 17:00:50 +0800

    4.27 更新，精准选点

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.MapOnTouchListener;
 import com.esri.android.map.MapView;
@@ -18,7 +23,6 @@ import com.esri.core.symbol.SimpleLineSymbol;
 import com.esri.core.symbol.SimpleMarkerSymbol;
 import com.gis.lib.Draw.DrawEvent;
 import com.gis.lib.Draw.DrawEventListener;
-import com.gis.lib.Draw.DrawTool;
 import com.gis.lib.Draw.TrackTool;
 import com.gis.lib.map.EMap;
 
@@ -31,116 +35,111 @@ public class MainActivity extends Activity implements DrawEventListener {
     private EMap emap = null;
     private MapView mapView = null;
     private GraphicsLayer graphicsLayer = null;
-    private DrawTool drawTool;
     public MyMapOnTouchListener mapDefaultOnTouchListener;//点击事件
-
+    ImageView imgHeart; // 准心
+    private int curOp = -1;
     TrackTool tool_line;
     TrackTool tool_point;
     List<Integer> list_line;
     List<Integer> list_point;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         context = this;
-        this.mapView = (MapView)this.findViewById(R.id.map);
-        emap=new EMap(mapView);
+        this.mapView = (MapView) this.findViewById(R.id.map);
+        emap = new EMap(mapView);
 
         emap.showWorldSatelliteLayer();//天地图影像
         // emap.showWorldVectorLayer();//天地图矢量
 
         //居中位置
-        emap.setCenter(120.09739062959324,30.266542846034017);
+        emap.setCenter(120.09739062959324, 30.266542846034017);
         //设置地图等级
         emap.setLevel(4);
         //画点
-       int id= emap.addPoint(120.09739062959324,30.266542846034017,new SimpleMarkerSymbol(Color.RED, 20, SimpleMarkerSymbol.STYLE.CIRCLE));
-
+        int id = emap.addPoint(120.09739062959324, 30.266542846034017, new SimpleMarkerSymbol(Color.RED, 20, SimpleMarkerSymbol.STYLE.CIRCLE));
+        //int id2= emap.getSelectedIndex(120.09739062959324,30.266542846034017); 一定范围选中返回id
 
         //画线
-        List<Double[]> myList=new ArrayList<Double[]>();
-        myList.add(new Double[]{120.10123,30.2321});
-        myList.add(new Double[]{123.1523,32.123321});
+        List<Double[]> myList = new ArrayList<Double[]>();
+        myList.add(new Double[]{120.10123, 30.2321});
+        myList.add(new Double[]{123.1523, 32.123321});
         emap.addPolyline(myList, new SimpleLineSymbol(Color.GREEN, 10));
 
 
         //画扇面
         LineSymbol mLineSymbol = new SimpleLineSymbol(Color.BLACK, 2);//边线样式
-        FillSymbol mFillSymbol =  new SimpleFillSymbol(Color.BLACK).setOutline(mLineSymbol).setAlpha(100);//面样式
-        Point center=new Point();
+        FillSymbol mFillSymbol = new SimpleFillSymbol(Color.BLACK).setOutline(mLineSymbol).setAlpha(100);//面样式
+        Point center = new Point();
         center.setX(120.10171884705784);
         center.setY(30.275667119589798);
-        emap.addSector(center, 1000, 0,60,mFillSymbol); //原点,半径（米）,开始角度,结束角度，样式
+        emap.addSector(center, 1000, 0, 60, mFillSymbol); //原点,半径（米）,开始角度,结束角度，样式
         // emap.clearSector();//删除扇面
         //emap.clearCircle();//删除圆
         // emap.clear();//删除其它
         //emap.clear(id);删点
 
         //画圆
-        FillSymbol mFillSymbol2 =  new SimpleFillSymbol(Color.BLUE).setOutline(mLineSymbol).setAlpha(100);//面样式
-        emap.addCircle(center, 1000,mFillSymbol2); //原点,半径（米）,样式
+        FillSymbol mFillSymbol2 = new SimpleFillSymbol(Color.BLUE).setOutline(mLineSymbol).setAlpha(100);//面样式
+        emap.addCircle(center, 1000, mFillSymbol2); //原点,半径（米）,样式
 
         //两点间距离（米）
-        double distance= emap.getDistance(119.2248,29.60848,119.2346,29.60893);
-        Log.e("distance:",distance+"");
+        double distance = emap.getDistance(119.2248, 29.60848, 119.2346, 29.60893);
+        Log.e("distance:", distance + "");
 
         //轨迹线-线
-        tool_line=new TrackTool(120.10123, 30.321, new SimpleLineSymbol(Color.BLACK, 5), mapView);
-        int i=tool_line.addLine(122.123321,30.1523);
-        int j=tool_line.addLine(124.123321,31.1523);
-        int k= tool_line.addLine(123.123321,32.1523);
-        list_line=new ArrayList();
+        tool_line = new TrackTool(120.10123, 30.321, new SimpleLineSymbol(Color.BLACK, 5), mapView);
+        int i = tool_line.addLine(122.123321, 30.1523);
+        int j = tool_line.addLine(124.123321, 31.1523);
+        int k = tool_line.addLine(123.123321, 32.1523);
+        list_line = new ArrayList();
         list_line.add(i);
         list_line.add(j);
         list_line.add(k);
-       // tool_line.clear_line(j);//删除线
-       // tool_line.clear_line(k);
+        // tool_line.clear_line(j);//删除线
+        // tool_line.clear_line(k);
 
         //轨迹线-点
-        SimpleMarkerSymbol mRedMarkerSymbol = new SimpleMarkerSymbol(Color.RED, 15, SimpleMarkerSymbol.STYLE.CIRCLE);
-        tool_point=new TrackTool(mRedMarkerSymbol,  mapView);
-        int x=tool_point.addPoint(120.10123, 30.321);
-        int m=tool_point.addPoint(122.123321,30.1523);
-        int n=tool_point.addPoint(124.123321,31.1523);
-        int q= tool_point.addPoint(123.123321,32.1523);
-        list_point=new ArrayList();
+        SimpleMarkerSymbol mRedMarkerSymbol = new SimpleMarkerSymbol(0xFFFF0000, 15, SimpleMarkerSymbol.STYLE.CIRCLE);
+        tool_point = new TrackTool(mRedMarkerSymbol, mapView);
+        int x = tool_point.addPoint(120.10123, 30.321);
+        int m = tool_point.addPoint(122.123321, 30.1523);
+        int n = tool_point.addPoint(124.123321, 31.1523);
+        int q = tool_point.addPoint(123.123321, 32.1523);
+        list_point = new ArrayList();
         list_point.add(x);
         list_point.add(m);
         list_point.add(n);
         list_point.add(q);
-
-      //  tool_point.clear_point(m); //删除某一点
-     //   tool_point.clear_point(n);
+        //  tool_point.clear_point(m); //删除某一点
+        //   tool_point.clear_point(n);
 
 
         //设置地图事件
         mapDefaultOnTouchListener = new MyMapOnTouchListener(emap.getMap().getContext(), emap.getMap());
         emap.setOnTouchListener(mapDefaultOnTouchListener);
 
-        /*ToolsOnClickListener toolsOnClickListener = new ToolsOnClickListener(context,drawTool,selectGraphic,emap.getMap());
-
-        Button btnDrawPoint = (Button)this.findViewById(R.id.btnDrawPoint);
-        btnDrawPoint.setOnClickListener(toolsOnClickListener);
-
-        Button btnDrawPolyline = (Button)this.findViewById(R.id.btnDrawPolyline);
-        btnDrawPolyline.setOnClickListener(toolsOnClickListener);
-
-
-        Button btnDrawPolygon = (Button)this.findViewById(R.id.btnDrawPolygon);
-        btnDrawPolygon.setOnClickListener(toolsOnClickListener);
-
-
-
-        Button btnDrawCircle = (Button)this.findViewById(R.id.btnDrawCircle);
-        btnDrawCircle.setOnClickListener(toolsOnClickListener);
-
-        Button btnFinshDraw = (Button)this.findViewById(R.id.btnFinshDraw);
-        btnFinshDraw.setOnClickListener(toolsOnClickListener);
-*/
-
-
-
+        //选点按钮
+        imgHeart = (ImageView) this.findViewById(R.id.imgHeart);
+        Button btnDrawPoint = (Button) this.findViewById(R.id.btnDrawPoint);
+        btnDrawPoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (curOp == -1) {
+                    curOp = 0;
+                    imgHeart.setVisibility(View.VISIBLE);
+                    return;
+                }
+                if (curOp == 0) {
+                    curOp = -1;
+                    imgHeart.setVisibility(View.GONE);
+                    return;
+                }
+            }
+        });
     }
 
     @Override
@@ -157,18 +156,17 @@ public class MainActivity extends Activity implements DrawEventListener {
         public MyMapOnTouchListener(Context context, MapView view) {
             super(context, view);
         }
+
         public boolean onSingleTap(MotionEvent e) {
             Point point = mapView.toMapPoint(e.getX(), e.getY());
-            //绘点
-            emap.addPoint(point.getX(), point.getY(), new SimpleMarkerSymbol(Color.RED, 20, SimpleMarkerSymbol.STYLE.CIRCLE));
             Log.e("点击", point.getX() + "," + point.getY()); //点击的经纬度
 
             int idx = tool_point.getSelectedIndex(e.getX(), e.getY());  //点中了轨迹线中的第几个点，返回-1则表示一个也没有点中
             if (idx != 0) {
-                for (int i=0;i<list_point.size();i++){
-                    if ( idx==list_point.get(i)){
+                for (int i = 0; i < list_point.size(); i++) {
+                    if (idx == list_point.get(i)) {
                         Log.e("点击了tool_point的第", i + "个点！");
-                        Toast.makeText(mapView.getContext(), "点击了tool_point的第"+i + "个点！", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(mapView.getContext(), "点击了tool_point的第" + i + "个点！", Toast.LENGTH_SHORT).show();
                     }
                 }
                 /*
@@ -176,12 +174,12 @@ public class MainActivity extends Activity implements DrawEventListener {
                 点中以后的一些操作，比如页面跳转等
                 ********
                 */
-            }else{
+            } else {
                 //线段点击
-                int j= tool_line.getSelectedIndexLine(e.getX(), e.getY());
-                for (int i=0;i<list_line.size();i++){
-                    if ( j==list_line.get(i)){
-                        Toast.makeText(mapView.getContext(), "点击了tool_line的第"+i + "段线！", Toast.LENGTH_SHORT).show();
+                int j = tool_line.getSelectedIndexLine(e.getX(), e.getY());
+                for (int i = 0; i < list_line.size(); i++) {
+                    if (j == list_line.get(i)) {
+                        Toast.makeText(mapView.getContext(), "点击了tool_line的第" + i + "段线！", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -193,6 +191,72 @@ public class MainActivity extends Activity implements DrawEventListener {
             Toast.makeText(mapView.getContext(), "长按事件", Toast.LENGTH_SHORT).show();
         }
 
+
+        //选中后操作
+        public boolean onDragPointerUp(MotionEvent from, MotionEvent to) {
+            if (curOp == -1) {
+                return super.onDragPointerUp(from, to);
+            }
+            if (curOp == 0) {
+                Point screenPt = new Point();
+                screenPt.setX(to.getX() - imgHeart.getWidth() / 2);
+                screenPt.setY(to.getY() - imgHeart.getHeight() / 2);
+                Point mapPt = mapView.toMapPoint(screenPt);
+
+                /*
+                ********
+                通过十字星拖动精确选中后的操作，
+                比如绘点
+                ********
+                */
+                int id = emap.addPoint(mapPt.getX(), mapPt.getY(), new SimpleMarkerSymbol(Color.RED, 20, SimpleMarkerSymbol.STYLE.CIRCLE));
+
+                return true;
+            }
+
+            return super.onDragPointerUp(from, to);
+        }
+
+
+        public boolean onDragPointerMove(MotionEvent from, MotionEvent to) {
+            if (curOp == -1) {
+                return super.onDragPointerMove(from, to);
+            }
+            if (curOp == 0) {
+                Point mapPt = mapView.toMapPoint(to.getX(), to.getY());
+                Point screenPt = mapView.toScreenPoint(mapPt);
+                imgHeart.layout((int) screenPt.getX() - imgHeart.getWidth(),
+                        (int) screenPt.getY() - imgHeart.getWidth(),
+                        (int) screenPt.getX(), (int) screenPt.getY());
+                return moveMap(from, to);
+            }
+            return super.onDragPointerMove(from, to);
+        }
+
+        private boolean moveMap(MotionEvent from, MotionEvent to) {
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            int screenWidth = displayMetrics.widthPixels;
+            int screenHeight = displayMetrics.heightPixels;
+            float fromX = from.getX();
+            float fromY = from.getY();
+            if (fromX < 15) {
+                from.setLocation(15, from.getY());
+                return super.onDragPointerMove(to, from);
+            }
+            if (fromX > screenWidth - 15) {
+                from.setLocation(screenWidth - 15, from.getY());
+                return super.onDragPointerMove(to, from);
+            }
+            if (fromY < 15) {
+                from.setLocation(from.getX(), 15);
+                return super.onDragPointerMove(to, from);
+            }
+            if (fromY > screenHeight - 15) {
+                from.setLocation(from.getX(), screenHeight - 15);
+                return super.onDragPointerMove(to, from);
+            }
+            return true;
+        }
     }
 
 }
